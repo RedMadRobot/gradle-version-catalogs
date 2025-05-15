6587c1b631b1bd259af3bb3c2a2e04e360a20292
a.tabolin <a.tabolin@redmadrobot.com>
2025-05-15 12:45:30 +0300

    [Configuration] add DisambiguationRule for org.jetbrains.kotlin.platform.type

--- build-logic/src/main/kotlin/convention.catalog.gradle.kts
+++ build-logic/src/main/kotlin/convention.catalog.gradle.kts
@@ -9,3 +9,18 @@ catalog {
         from(files("libs.versions.toml"))
     }
 }
+
+// если есть несколько вариантов атрибута "org.jetbrains.kotlin.platform.type", то первым выбираем "androidJvm"
+dependencies.attributesSchema.attribute(Attribute.of("org.jetbrains.kotlin.platform.type", String::class.java)) {
+    disambiguationRules.add(PreferAndroidJvmDisambiguationRule::class.java)
+}
+
+abstract class PreferAndroidJvmDisambiguationRule : AttributeDisambiguationRule<String> {
+    override fun execute(details: MultipleCandidatesDetails<String>) {
+        if ("androidJvm" in details.candidateValues) {
+            details.closestMatch("androidJvm")
+        } else if ("jvm" in details.candidateValues) {
+            details.closestMatch("jvm")
+        }
+    }
+}
